d7cffc34b3fa665f7bc74ce858cf26250da03bfc
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-07-26 16:31:07 +0300

    add getter and setter

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс представления категории продуктов."""
 
@@ -10,7 +13,27 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def add_product(self, product: Product):
+        self.__products = []
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def get_product(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product['name']}, {product['price']} руб. Остаток: {product['quantity']} шт."
+
+        return product_str
